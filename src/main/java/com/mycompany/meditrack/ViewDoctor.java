8cd5e9177dac35046426754ea2e58da50427bda9
Gauri Kshirsagar <gaurikshirsagr5@gmail.com>
2026-05-07 22:59:28 +0530

    Update ViewDoctor.java

--- src/main/java/com/mycompany/meditrack/ViewDoctor.java
+++ src/main/java/com/mycompany/meditrack/ViewDoctor.java
@@ -131,7 +131,7 @@ public class ViewDoctor extends javax.swing.JFrame {
         try{
             DefaultTableModel model=(DefaultTableModel)Table.getModel();
           Class.forName("com.mysql.cj.jdbc.Driver");
-          Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Hospital","root","gauri@mysql");
+          Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Hospital","root","yourPassword");
             PreparedStatement pst=con.prepareStatement("SELECT * FROM doctor");
                  ResultSet rs=pst.executeQuery();
           while(rs.next()){
