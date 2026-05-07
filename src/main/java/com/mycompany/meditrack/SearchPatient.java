be52207127babded580d8cabcc5a9eb2d72ae88a
Gauri Kshirsagar <gaurikshirsagr5@gmail.com>
2026-05-07 22:58:36 +0530

    Update database connection password in SearchPatient

--- src/main/java/com/mycompany/meditrack/SearchPatient.java
+++ src/main/java/com/mycompany/meditrack/SearchPatient.java
@@ -151,7 +151,7 @@ public class SearchPatient extends javax.swing.JFrame {
     try{
             DefaultTableModel model=(DefaultTableModel)Table.getModel();
           Class.forName("com.mysql.cj.jdbc.Driver");
-          Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Hospital","root","gauri@mysql");
+          Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Hospital","root","yourPassword");
             PreparedStatement pst=con.prepareStatement("SELECT * FROM patient WHERE ID=?");
           pst.setString(1, patientId);
           ResultSet rs=pst.executeQuery();
